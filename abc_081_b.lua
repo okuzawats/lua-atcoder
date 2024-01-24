c879e1069645ae57b2206ec7e3faf932de7688d0
okuzawats <okuzawats@gmail.com>
2024-01-24 22:34:36 +0900

    [AC] ABC 081 B

---
+++ abc_081_b.lua
@@ -0,0 +1,16 @@
+n = io.read("*n")
+a = {}
+for i = 1, n do a[i] = io.read("*n") end
+
+c = 0
+while true do
+  f = false
+  for i = 1, n do
+    if a[i] % 2 > 0 then f = true end
+  end
+  if f then break end
+  for j = 1, n do a[j] = a[j] / 2 end
+  c = c + 1
+end
+
+print(c)
