c77d9ff6e7c386604857d8dd8e119cd0521cebe2
okuzawats <okuzawats@gmail.com>
2024-01-31 06:27:33 +0900

    [AC] ABC 087 B

---
+++ abc_087_b.lua
@@ -0,0 +1,16 @@
+a = io.read("*n")
+b = io.read("*n")
+c = io.read("*n")
+x = io.read("*n")
+
+s = 0
+for i = 0, a  do
+  for j = 0, b do
+    for k = 0, c do
+      if i * 500 + j * 100 + k * 50 == x then
+        s = s + 1
+      end
+    end
+  end
+end
+print(s)
