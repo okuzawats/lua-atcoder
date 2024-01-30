27eec4a21bc921535ca1fe9539f815f054b620e3
okuzawats <okuzawats@gmail.com>
2024-01-31 06:33:30 +0900

    [AC] forループをネストさせずに書くように修正

--- abc_087_b.lua
+++ abc_087_b.lua
@@ -1,13 +1,9 @@
 a, b, c, x = io.read("*n", "*n", "*n", "*n")
 
 s = 0
-for i = 0, a  do
-  for j = 0, b do
-    for k = 0, c do
-      if i * 500 + j * 100 + k * 50 == x then
-        s = s + 1
-      end
-    end
+for i = 0, a  do for j = 0, b do for k = 0, c do
+  if i * 500 + j * 100 + k * 50 == x then
+    s = s + 1
   end
-end
+end end end
 print(s)
