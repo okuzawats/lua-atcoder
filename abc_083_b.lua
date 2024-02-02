e145bb319f5492ac0cc73e9772703cab45bcd6c3
okuzawats <okuzawats@gmail.com>
2024-02-02 13:48:08 +0900

    [AC] ABC 083 B

---
+++ abc_083_b.lua
@@ -0,0 +1,13 @@
+n, a, b = io.read("n", "n", "n")
+c = 0
+for i = 1, n do
+  s = tostring(i)
+  sum = 0
+  for j = 1, #s do
+    sum = sum + tonumber(string.sub(s, j, j))
+  end
+  if b >= sum and sum >= a then
+    c = c + i
+  end
+end
+print(c)
