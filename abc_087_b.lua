c66805ae1116323dfadca46b5220e83fc67c2c69
okuzawats <okuzawats@gmail.com>
2024-02-03 16:48:30 +0900

    [AC] io.readの書き方を修正

--- abc_087_b.lua
+++ abc_087_b.lua
@@ -1,9 +1,7 @@
-a, b, c, x = io.read("*n", "*n", "*n", "*n")
+a, b, c, x = io.read("n", "n", "n", "n")
 
 s = 0
 for i = 0, a  do for j = 0, b do for k = 0, c do
-  if i * 500 + j * 100 + k * 50 == x then
-    s = s + 1
-  end
+  if i * 500 + j * 100 + k * 50 == x then s = s + 1 end
 end end end
 print(s)
