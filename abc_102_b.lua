9a086ceb95300ce40b55053dabe2b7c1a7dfb0df
okuzawats <okuzawats@gmail.com>
2024-02-04 15:00:39 +0900

    [AC] table.sortを用いたソート処理により修正

--- abc_102_b.lua
+++ abc_102_b.lua
@@ -1,12 +1,5 @@
 n = io.read("n")
 a = {}
 for i = 1, n do a[i] = io.read("n") end
-
-min = 10^9
-max = 0
-for i = 1, #a do
-  if a[i] < min then min = a[i] end
-  if a[i] > max then max = a[i] end
-end
-
-print(max - min)
+table.sort(a)
+print(a[n] - a[1])
