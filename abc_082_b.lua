c48a30d5ad876d379ee38e8799393f4198872242
okuzawats <okuzawats@gmail.com>
2024-02-06 06:30:48 +0900

    [AC] 変数宣言を1行で実行するように修正

--- abc_082_b.lua
+++ abc_082_b.lua
@@ -1,6 +1,5 @@
-s1, s2 = io.read(), {}
+s1, s2, t1, t2 = io.read(), {}, io.read(), {}
 for i = 1, #s1 do table.insert(s2, s1:sub(i, i)) end
-t1, t2 = io.read(), {}
 for j = 1, #t1 do table.insert(t2, t1:sub(j, j)) end
 
 table.sort(s2)
