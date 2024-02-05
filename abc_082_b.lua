c14e41e3cfc781fbbe2b5a05a2879b160cc3dd8c
okuzawats <okuzawats@gmail.com>
2024-02-06 06:02:48 +0900

    [AC] ABC 082 B

---
+++ abc_082_b.lua
@@ -0,0 +1,9 @@
+s1, s2 = io.read(), {}
+for i = 1, #s1 do table.insert(s2, s1:sub(i, i)) end
+t1, t2 = io.read(), {}
+for j = 1, #t1 do table.insert(t2, t1:sub(j, j)) end
+
+table.sort(s2)
+table.sort(t2, function(a, b) return a > b end)
+
+print(table.concat(s2, "") < table.concat(t2, "") and "Yes" or "No")
