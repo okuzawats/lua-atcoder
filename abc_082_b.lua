b5d9bb437bb854846a9cd5fa976922fe56042aa3
okuzawats <okuzawats@gmail.com>
2024-02-06 06:04:02 +0900

    [AC] table.concatの引数を削減

--- abc_082_b.lua
+++ abc_082_b.lua
@@ -6,4 +6,4 @@ for j = 1, #t1 do table.insert(t2, t1:sub(j, j)) end
 table.sort(s2)
 table.sort(t2, function(a, b) return a > b end)
 
-print(table.concat(s2, "") < table.concat(t2, "") and "Yes" or "No")
+print(table.concat(s2) < table.concat(t2) and "Yes" or "No")
