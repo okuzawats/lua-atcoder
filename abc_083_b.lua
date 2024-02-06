54497c1c18c188db7011ce9b470da7c32dbcbf50
okuzawats <okuzawats@gmail.com>
2024-02-07 05:41:54 +0900

    [AC] 不要なコードの刈り込み

--- abc_083_b.lua
+++ abc_083_b.lua
@@ -1,13 +1,8 @@
 n, a, b = io.read("n", "n", "n")
 c = 0
 for i = 1, n do
-  s = tostring(i)
-  sum = 0
-  for j = 1, #s do
-    sum = sum + tonumber(s:sub(j, j))
-  end
-  if b >= sum and sum >= a then
-    c = c + i
-  end
+  s, sum = tostring(i), 0
+  for j = 1, #s do sum = sum + s:sub(j, j) end
+  if b >= sum and sum >= a then c = c + i end
 end
 print(c)
