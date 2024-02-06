da2a0c5d35022ee58f18c77b61004891e174813a
okuzawats <okuzawats@gmail.com>
2024-02-07 05:58:57 +0900

    [AC] ABC 068 B

--- abc_068_b.lua
+++ abc_068_b.lua
@@ -1,15 +1,3 @@
-n, a, c = io.read("n"), 0, 0
-
-for i = 1, n do
-  local num, div = i, 0
-  while num % 2 == 0 do
-    num = num / 2
-    div = div + 1
-  end
-  if div > c then
-    a = i
-    c = div
-  end
-end
-
+n, a = io.read("n"), 1
+while a * 2 <= n do a = a * 2 end
 print(a)
