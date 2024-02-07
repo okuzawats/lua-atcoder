75f10b5893f91ce2aee6596f04dc20b01affe127
okuzawats <okuzawats@gmail.com>
2024-02-07 13:50:01 +0900

    [AC] 2重ループを1重ループに修正

--- abc_105_b.lua
+++ abc_105_b.lua
@@ -1,10 +1,8 @@
 n = io.read("n")
 for i = 0, n // 4 do
-  for j = 0, n // 7 do
-    if i * 4 + j * 7 == n then
-      print("Yes")
-      return
-    end
+  if (n - i * 4) % 7 < 1 then
+    print("Yes")
+    return
   end
 end
 print("No")
