af206279958a51e7bc072302727ab34dfc14a886
okuzawats <okuzawats@gmail.com>
2024-02-09 09:53:09 +0900

    [未提出] 変数名が衝突して正しく動作しなかったため変数をリネーム

--- abc_175_b.lua
+++ abc_175_b.lua
@@ -3,19 +3,20 @@ l = {}
 for i = 1, n do
   l[i] = io.read("n")
 end
-c = 0
+
+count = 0
 for i = 1, n - 2 do
   for j = i + 1, n - 1 do
     for k = j + 1, n do
-      a, b, c = l[i], l[j], l[k]
+      local a, b, c = l[i], l[j], l[k]
       if a == b or b == c or c == a then
         goto continue
       end
       if a < b + c and b < c + a and c < a + b then
-        c = c + 1
+        count = count + 1
       end
       ::continue::
     end
   end
 end
-print(c)
+print(count)
