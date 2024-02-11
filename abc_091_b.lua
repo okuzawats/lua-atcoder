4716c3fbc0133a56b9362a60e21c4b1baa33206b
okuzawats <okuzawats@gmail.com>
2024-02-12 06:21:48 +0900

    [AC] nilの判定の順序を逆に修正

--- abc_091_b.lua
+++ abc_091_b.lua
@@ -2,10 +2,10 @@ n = io.read("n", "l")
 s = {}
 for i = 1, n do
   w = io.read()
-  if s[w] == nil then
-    s[w] = 1
-  else
+  if s[w] then
     s[w] = s[w] + 1
+  else
+    s[w] = 1
   end
 end
 
@@ -13,19 +13,19 @@ m = io.read("n", "l")
 t = {}
 for i = 1, m do
   w = io.read()
-  if t[w] == nil then
-    t[w] = 1
-  else
+  if t[w] then
     t[w] = t[w] + 1
+  else
+    t[w] = 1
   end
 end
 
 a = 0
 for k, v in pairs(s) do
-  if t[k] == nil then
-    a = math.max(a, s[k])
-  else
+  if t[k] then
     a = math.max(a, s[k] - t[k])
+  else
+    a = math.max(a, s[k])
   end
 end
 print(a)
