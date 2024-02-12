a79482effe7513055353deb362c05dc25579eb73
okuzawats <okuzawats@gmail.com>
2024-02-13 06:06:46 +0900

    [AC] 貪欲法を用いた解法に修正

--- abc_049_c.lua
+++ abc_049_c.lua
@@ -1,6 +1,27 @@
 s = io.read()
- :gsub("eraser", "")
- :gsub("erase", "")
- :gsub("dreamer", "")
- :gsub("dream", "")
-print(s == "" and "YES" or "NO")
+s = s:reverse()
+
+w = { "dream", "dreamer", "erase", "eraser" }
+for i = 1, #w do w[i] = w[i]:reverse() end
+
+f = true
+while true do
+  f2 = false
+  for j = 1, #w do
+    w2 = w[j]
+    if s:sub(1, #w2) == w2 then
+      f2 = true
+      s = s:sub(#w2 + 1, #s)
+      break
+    end
+  end
+  if s == "" then
+    break
+  end
+  if not f2 then
+    f = false
+    break
+  end
+end
+
+print(f and "YES" or "NO")
