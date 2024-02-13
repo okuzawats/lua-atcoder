663d8f92c783ab25a4558891f742d13fff98e8b6
okuzawats <okuzawats@gmail.com>
2024-02-14 05:35:23 +0900

    [AC] AC以外にUpperCaseを含まない条件を追加

--- abc_104_b.lua
+++ abc_104_b.lua
@@ -1,12 +1,21 @@
 s = io.read()
-a = s:sub(1, 1) == "A"
+f1 = s:sub(1, 1) == "A"
 
-f, c, p = false, 0, s:sub(3, #s - 1)
+f2, c, p = false, 0, s:sub(3, #s - 1)
 for i = 1, #p do
   if p:sub(i, i) == "C" then
     c = c + 1
-    f = c == 1
+    f2 = c == 1
   end
 end
 
-print((a and f) and "AC" or "WA")
+u = 0
+for i = 1, #s do
+  b = s:sub(i, i):byte() - 96
+  if b < 0 then
+    u = u + 1
+  end
+end
+f3 = u == c + 1
+
+print((f1 and f2 and f3) and "AC" or "WA")
