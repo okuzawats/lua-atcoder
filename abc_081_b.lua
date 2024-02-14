1d94bcce8b8440d7c4b12b2b34d363626dec97b4
okuzawats <okuzawats@gmail.com>
2024-02-15 06:13:05 +0900

    [AC] 除算に//を使用して修正

--- abc_081_b.lua
+++ abc_081_b.lua
@@ -1,5 +1,4 @@
-n = io.read("n")
-a = {}
+a, n = {}, io.read("n")
 for i = 1, n do a[i] = io.read("n") end
 
 c = 0
@@ -9,8 +8,7 @@ while true do
     if a[i] % 2 > 0 then f = true end
   end
   if f then break end
-  for j = 1, n do a[j] = a[j] / 2 end
+  for j = 1, n do a[j] = a[j] // 2 end
   c = c + 1
 end
-
 print(c)
