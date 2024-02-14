a7913e9f231b60dcd72abfa065393dda86635ffc
okuzawats <okuzawats@gmail.com>
2024-02-15 06:26:56 +0900

    [AC] 不要な計算をスキップするよう、breakを追加

--- abc_081_b.lua
+++ abc_081_b.lua
@@ -5,7 +5,7 @@ c = 0
 while true do
   f = false
   for i = 1, n do
-    if a[i] % 2 > 0 then f = true end
+    if a[i] % 2 > 0 then f = true; break end
   end
   if f then break end
   for j = 1, n do a[j] = a[j] // 2 end
