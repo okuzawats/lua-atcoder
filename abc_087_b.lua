f97ba471af7701fffd491420920404cf8e223669
okuzawats <okuzawats@gmail.com>
2024-02-15 13:56:02 +0900

    [AC] for文をネストして書くように修正

--- abc_087_b.lua
+++ abc_087_b.lua
@@ -1,7 +1,14 @@
 a, b, c, x = io.read("n", "n", "n", "n")
 
-s = 0
-for i = 0, a  do for j = 0, b do for k = 0, c do
-  if i * 500 + j * 100 + k * 50 == x then s = s + 1 end
-end end end
-print(s)
+count = 0
+for i = 0, a  do
+  for j = 0, b do
+    for k = 0, c do
+      sum = 500 * i + 100 * j + 50 * k
+      if sum == x then
+        count = count + 1
+      end
+    end
+  end
+end
+print(count)
