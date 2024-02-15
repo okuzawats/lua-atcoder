f6499993ab7c6bcc7d46c6d3e2c984c8c493698b
okuzawats <okuzawats@gmail.com>
2024-02-16 06:01:11 +0900

    [AC] ifのブロックを改行

--- abc_083_b.lua
+++ abc_083_b.lua
@@ -1,8 +1,13 @@
 n, a, b = io.read("n", "n", "n")
-c = 0
+
+count = 0
 for i = 1, n do
   s, sum = tostring(i), 0
-  for j = 1, #s do sum = sum + s:sub(j, j) end
-  if b >= sum and sum >= a then c = c + i end
+  for j = 1, #s do
+    sum = sum + s:sub(j, j)
+  end
+  if b >= sum and sum >= a then
+    count = count + i
+  end
 end
-print(c)
+print(count)
