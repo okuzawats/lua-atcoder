259915d8b8a05ff9e82c90ea74ed561c66273890
okuzawats <okuzawats@gmail.com>
2024-02-16 05:46:18 +0900

    [AC] ifのブロックを改行するように修正

--- abc_081_b.lua
+++ abc_081_b.lua
@@ -1,14 +1,23 @@
 a, n = {}, io.read("n")
-for i = 1, n do a[i] = io.read("n") end
+for i = 1, n do
+  a[i] = io.read("n")
+end
 
 c = 0
 while true do
   f = false
   for i = 1, n do
-    if a[i] % 2 > 0 then f = true; break end
+    if a[i] % 2 > 0 then
+      f = true
+      break
+    end
   end
-  if f then break end
-  for j = 1, n do a[j] = a[j] // 2 end
+  if f then
+    break
+  end
+  for j = 1, n do
+    a[j] = a[j] // 2
+  end
   c = c + 1
 end
 print(c)
