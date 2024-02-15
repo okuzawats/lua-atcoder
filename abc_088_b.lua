d5f0fefc0886fccb2e91ca0f6a7068f64deffe09
okuzawats <okuzawats@gmail.com>
2024-02-16 06:21:21 +0900

    [AC] 変数をリネーム

--- abc_088_b.lua
+++ abc_088_b.lua
@@ -1,17 +1,16 @@
-n = io.read("n")
-a = {}
+a, n = {}, io.read("n")
 for i = 1, n do
   table.insert(a, io.read("n"))
 end
 
 table.sort(a, function(a, b) return a > b end)
 
-c = 0
+count = 0
 for i = 1, #a do
   if i % 2 > 0 then
-    c = c + a[i]
+    count = count + a[i]
   else
-    c = c - a[i]
+    count = count - a[i]
   end
 end
-print(c)
+print(count)
