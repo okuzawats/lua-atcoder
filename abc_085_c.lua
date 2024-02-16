ab2710abd21f6f9da033083519addc5be5420221
okuzawats <okuzawats@gmail.com>
2024-02-17 05:55:25 +0900

    [AC] forのブロックを改行するように修正

--- abc_085_c.lua
+++ abc_085_c.lua
@@ -1,9 +1,11 @@
 n, y = io.read("n", "n")
-for i = 0, n do for j = 0, n - i do
-  k = n - i - j
-  if 10000 * i + 5000 * j + 1000 * k == y then
-    print(i.." "..j.." "..k)
-    os.exit()
+for i = 0, n do
+  for j = 0, n - i do
+    k = n - i - j
+    if 10000 * i + 5000 * j + 1000 * k == y then
+      print(i.." "..j.." "..k)
+      os.exit()
+    end
   end
-end end
+end
 print("-1 -1 -1")
